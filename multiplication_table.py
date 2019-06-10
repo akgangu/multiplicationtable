e2113f3a59782121e522a573f99833f08ae5806b
akgangu <adithya.kiran@gmail.com>
2019-06-10 16:43:39 -0500

    multiplication table

---
+++ multiplication_table.py
@@ -0,0 +1,7 @@
+def multiplication_table(num):
+    tens_place = num / 10
+    ones_place = num % 10
+    first_set = [  (tens_place * i) + (ones_place * i / 10) for i in range(1,11) ]
+    second_set = [ (ones_place * i % 10) for i in range(1,11) ]
+    for idx, i, j in enumerate(zip(first_set, second_set), 1):
+        print "%s * %s = %s" % (num, idx, str(i) + str(j))
